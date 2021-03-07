6500ebd1fca39390cbc850ef159fc74055360c42
konstantinosaulenko <constantin.osaulenko@yandex.ru>
2021-03-07 18:04:08 +0300

    feat: footer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Header/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 export default {
   name: 'App',
   components: {
     Header,
+    Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,67 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="footer__row">
+        <div class="footer__col">
+          <div class="footer__logo">i-vtec</div>
+        </div>
+
+        <div class="footer__col">
+          <div class="footer__col-list">
+            <a href="#" class="footer__col-item footer__col-item_title">Каталог</a>
+            <a href="#" class="footer__col-item">Моторы</a>
+            <a href="#" class="footer__col-item">Запчасти</a>
+            <a href="#" class="footer__col-item">Расходные материалы</a>
+            <a href="#" class="footer__col-item">Комплектующие</a>
+          </div>
+        </div>
+
+        <div class="footer__col">
+          <div class="footer__col-list">
+            <a href="#" class="footer__col-item footer__col-item_title">Информация</a>
+            <a href="#" class="footer__col-item">Условия оплаты</a>
+            <a href="#" class="footer__col-item">Условия доставки</a>
+            <a href="#" class="footer__col-item">Гарантии на товар</a>
+            <a href="#" class="footer__col-item">Акции</a>
+          </div>
+        </div>
+
+        <div class="footer__col">
+          <div class="footer__col-list">
+            <a href="#" class="footer__col-item footer__col-item_title">Будь в курсе новостей и изменений </a>
+            <div class="footer__col-item footer__col-item-search">
+              <input type="text" class="footer__col-item-search-input" placeholder="email">
+            </div>
+            <div class="footer__col-item footer__col-item-contacts">
+              <div class="footer__col-item-contacts-title">Контакты</div>
+              <div class="footer__col-item-contacts-text">
+                <div class="footer__col-item-contacts-text-row">123317, г. Москва,</div>
+                <div class="footer__col-item-contacts-text-row">Пресненская набережная, д. 10, этаж 47,</div>
+                <div class="footer__col-item-contacts-text-row">Комплекс “Башня на Набережной”, блок С</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer__row">
+        <div class="footer__policy">
+          <div class="footer__policy-text">
+            2021 © изделия и аксессуары для автомобилей Honda
+          </div>
+          <div class="footer__policy-text">
+            Yamaha, Suzuki, Honda, Tohatsu
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: 'Footer',
+    components: {}
+  }
+</script>
+
+<style></style>
