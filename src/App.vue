9e6e8d161eb6891705a537b906087d109700dce9
konstantinosaulenko <constantin.osaulenko@yandex.ru>
2021-03-07 19:25:58 +0300

    feat: catalog

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,51 @@
 <template>
   <div id="app">
     <Header/>
+
+    <main>
+      <section class="section catalog">
+        <div class="container">
+          <h2 class="section__title">Каталог</h2>
+          <div class="catalog__cards cards">
+            <Card
+              v-for="catalogItem in catalogItems"
+              :key="catalogItem.title"
+              :card_data='catalogItem'
+            />
+          </div>
+        </div>
+      </section>
+
+    </main>
+
     <Footer/>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header'
+import Card from '@/components/Card'
 import Footer from '@/components/Footer'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      catalogItems: [
+        { img: 'default-image.png', title: 'Воздухозаборники', class: 'catalog' },
+        { img: 'default-image.png', title: 'Интерьер', class: 'catalog' },
+        { img: 'default-image.png', title: 'Колесные гайки', class: 'catalog' },
+        { img: 'default-image.png', title: 'Пластины противоотливные', class: 'catalog' },
+        { img: 'default-image.png', title: 'Пластины радиатора', class: 'catalog' },
+        { img: 'default-image.png', title: 'Рычаги подвески', class: 'catalog' },
+        { img: 'default-image.png', title: 'Усилители кузова', class: 'catalog' },
+        { img: 'default-image.png', title: 'Экстерьер', class: 'catalog' },
+      ]
+    }
+  },
   components: {
     Header,
+    Card,
     Footer
   }
 }
---
+++ src/components/Card.vue
@@ -0,0 +1,30 @@
+<template>
+  <a href="#" 
+    class="card"
+    :class="`${card_data.class}__card`"
+  >
+    <img 
+      :src="require(`@/assets/img/${card_data.img}`)" 
+      alt="" 
+      :class="`${card_data.class}__card-img`"
+    >
+    <div :class="`${card_data.class}__card-title`">{{ card_data.title }}</div>
+  </a>
+</template>
+
+<script>
+  export default {
+    name: 'Card',
+    components: {},
+    props: {
+      card_data: {
+        type: Object,
+        default () {
+          return {}
+        }
+      },
+    }
+  }
+</script>
+
+<style></style>
